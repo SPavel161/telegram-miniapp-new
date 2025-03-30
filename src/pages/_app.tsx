31d07c65a2ac3aa45b759df61f1a866bbc39f16a
avel161 <safronov.586608@gmail.com>
2025-03-30 19:21:08 +0300

    Remove environment variables from vercel.json and update TypeScript config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
+// @ts-ignore
 import WebApp from '@twa-dev/sdk';
 
 export default function App({ Component, pageProps }: AppProps) {
